e0c9d94808db7fb59cb4db8405c7cfe389d402b8
Umut Bektaş <umutbektass.78@hotmail.com>
2024-12-04 17:41:09 +0300

    Added Title component to display a "Game Over" heading.
    Integrated a success image with custom styling inside a circular container.
    
    Updated layout to center align content using rootScreen and imageContainer styles.

--- app/screens/gameoverscreen/GameOverScreen.tsx
+++ app/screens/gameoverscreen/GameOverScreen.tsx
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Title from '@/app/components/ui/Title'
+import Color from '@/app/contants/color'
 
 const GameOverScreen = () => {
   return (
-    <View>
-      <Text>GameOverScreen</Text>
+    <View style={styles.rootScreen}> 
+     <Title>Game Over</Title>
+     <View style={styles.imageContainer}>
+    <Image style={styles.image} source={require('../../../assets/images/success.png')}/>
+     </View>
     </View>
   )
 }
 
 export default GameOverScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  rootScreen:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    padding:24
+  },
+  imageContainer:{
+    width:300,
+    height:300,
+    borderRadius:150,
+    borderWidth:3,
+    borderColor:Color.primary800,
+    overflow:'hidden',
+    margin:36
+  },
+  image:{
+    width:'100%',
+    height:'100%',
+   }
+})
